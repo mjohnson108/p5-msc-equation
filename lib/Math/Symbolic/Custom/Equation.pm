c88b23edcc626bc73dbed38c255a3ab080a0d464
mjohnson <mjohnson@cpan.org>
2025-02-27 10:03:46 +0000

    first commit

---
+++ lib/Math/Symbolic/Custom/Equation.pm
@@ -0,0 +1,104 @@
+package Math::Symbolic::Custom::Equation;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Math::Symbolic::Custom::Equation - The great new Math::Symbolic::Custom::Equation!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Math::Symbolic::Custom::Equation;
+
+    my $foo = Math::Symbolic::Custom::Equation->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Matt Johnson, C<< <mjohnson at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-math-symbolic-custom-equation at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=Math-Symbolic-Custom-Equation>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Math::Symbolic::Custom::Equation
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=Math-Symbolic-Custom-Equation>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/Math-Symbolic-Custom-Equation>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/Math-Symbolic-Custom-Equation>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is copyright (c) 2025 by Matt Johnson.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+
+=cut
+
+1; # End of Math::Symbolic::Custom::Equation
